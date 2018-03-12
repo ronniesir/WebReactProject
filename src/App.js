aee96aa583caa3c859f57a2b228bcdac48c0b8d6
ronnie.jing <ronnie.jing@gtedx.com>
2018-03-12 17:58:26 +0800

    测试提交 1、提交1 2、提交2

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+/**
+ *  功能名称：
+ *  功能描述：
+ *  创建人：
+ *  创建时间：
+ *  创建人邮箱
+ *  修改人：
+ *  修改人邮箱：
+ *  修改时间：
+ *  修改原因：
+ */
+class App extends Component {
+
+    /**
+     *   方法名称：
+     *  方法描述：
+     *                  传入参数：
+     *                  返回参数：
+     *  创建人：
+     *  创建时间：
+     *  创建人邮箱
+     *  修改人：
+     *  修改人邮箱：
+     *  修改时间：
+     *  修改原因：
+     *
+     */
+    customMethod(data) {
+        if (true) {
+
+        } else {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
